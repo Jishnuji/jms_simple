49d464ff33ce419adcd5c1c7d11c791187889756
Jishnuji <jishnuji@gmail.com>
2021-10-26 13:38:03 +0700

    Embedded  broker

--- src/main/java/TransactedProducer.java
+++ src/main/java/TransactedProducer.java
@@ -1,14 +1,22 @@
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+
 import javax.jms.*;
 import java.time.Duration;
 import java.time.Instant;
 
 public class TransactedProducer {
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws Exception {
         TransactedProducer.sendTransacted();
     }
-    public static void sendTransacted() throws JMSException {
-        ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+    public static void sendTransacted() throws Exception {
+        BrokerService broker = new BrokerService();
+        broker.setBrokerName("service");
+        broker.addConnector("tcp://localhost:61616");
+        broker.start();
+
+        ActiveMQConnectionFactory cf =
+                new ActiveMQConnectionFactory("vm://service");
         Connection connection = cf.createConnection();
         connection.start();
         Session session =
@@ -16,9 +24,10 @@ public class TransactedProducer {
 
         Destination destination = session.createQueue("message.queue");
         MessageProducer producer = session.createProducer(destination);
+        MessageConsumer consumer = session.createConsumer(destination);
 
+        // Producer
         Instant start = Instant.now();
-
         for (int i = 0; i <= 100_000; i++) {
             Message message = session.createTextMessage("message " + i);
             producer.send(message);
@@ -26,12 +35,25 @@ public class TransactedProducer {
                 session.commit();
             }
         }
-
         Instant finish = Instant.now();
         long elapsed = Duration.between(start, finish).toMillis();
         System.out.println("Прошло времени, мс: " + elapsed);
 
+        // Consumer
+        Instant start2 = Instant.now();
+        for (int i = 0; i <= 100_000; i++) {
+            TextMessage textMessage = (TextMessage) consumer.receive();
+            if (i != 0 && i % 1000 == 0){
+                session.commit();
+            }
+        }
+//        session.commit();
+        Instant finish2 = Instant.now();
+        long elapsed2 = Duration.between(start2, finish2).toMillis();
+        System.out.println("Прошло времени, мс: " + elapsed2);
+
         session.close();
         connection.close();
+        broker.stop();
     }
 }
